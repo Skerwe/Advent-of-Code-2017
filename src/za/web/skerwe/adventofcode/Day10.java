ec9a7a30556938c7af3760e144243b5cb7b83572
Quintin Henn <quintin.henn@momentum.co.za>
2017-12-15 15:45:47 +0200

    Refactored Day 10

--- src/za/web/skerwe/adventofcode/Day10.java
+++ src/za/web/skerwe/adventofcode/Day10.java
@@ -1,16 +1,83 @@
 package za.web.skerwe.adventofcode;
 
+import java.io.IOException;
+import za.web.skerwe.util.InputFileReader;
+
 public class Day10 {
 
-    private static byte[] standardLengthSuffixValues = {17, 31, 73, 47, 23};
+  public static byte[] standardLengthSuffixValues = {17, 31, 73, 47, 23};
 
     public static void main(String[] args) {
-        String input = "120,93,0,90,5,80,129,74,1,165,204,255,254,2,50,113";
+      try {
+        String[] input = InputFileReader.readInputFile("input/day10.txt");
+        Day10 challenge = new Day10();
 
-        System.out.println("\nKnot Hash = " + new Day10().process(combine(input.getBytes(), standardLengthSuffixValues)));
+        System.out.println(String.format("Result of multiplying the first two numbers in the list = %s", challenge.part1(input[0])));
+        System.out.println(String.format("Knot Hash = %s", challenge.part2(input[0])));
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 
-    public static int[] combine(byte[] a, byte[] b) {
+    public int part1(String input) {
+      String[] parts = input.split(",");
+      int[] lengths = new int[parts.length];
+      for (int i = 0; i < parts.length; i++) {
+        lengths[i] = Integer.parseInt(parts[i]);
+      }
+
+      int[] hashString = makeHashString();
+
+      Model model = new Model();
+      model.setLengths(lengths);
+      model.setHashString(hashString);
+
+      method(model);
+      return hashString[0] * hashString[1];
+    }
+
+    public String part2(String input) {
+      int[] lengths = Day10.combineByteArrayToIntArray(input.getBytes(), Day10.standardLengthSuffixValues);
+      int[] hashString = makeHashString();
+
+      Model model = new Model();
+      model.setLengths(lengths);
+      model.setHashString(hashString);
+
+      int round = 0;
+      while (round < 64) {
+        method(model);
+        round++;
+      }
+
+      int[] sparseHash = new int[16];
+      for (int hash = 0; hash < 16; hash++) {
+          int value = 0;
+          for (int i = (hash * 16), j = (hash * 16) + 16; i < j; i++) {
+              value = value ^ model.hashString[i];
+          }
+          sparseHash[hash] = value;
+      }
+
+      StringBuilder buffer = new StringBuilder();
+      for (int v : sparseHash) {
+          String h = Integer.toHexString(v);
+          buffer.append(h.length() == 1 ? ("0" + h) : h);
+      }
+
+      return buffer.toString();
+    }
+
+    private int[] makeHashString() {
+      int[] hashString = new int[256];
+      for (int i = 0; i < hashString.length; i++) {
+        hashString[i] = i;
+      }
+      return hashString;
+    }
+
+    public static int[] combineByteArrayToIntArray(byte[] a, byte[] b) {
         int length = a.length + b.length;
         int[] result = new int[length];
         for (int i = 0; i < a.length; i++) {
@@ -22,58 +89,72 @@ public class Day10 {
         return result;
     }
 
-    private String process(int[] lengths) {
-        int currentPosition = 0, skipSize = 0, count = 0;
+    public void method(Model model) {
 
-        int[] hashString = new int[256];
-        for (int i = 0; i < hashString.length; i++) {
-            hashString[i] = i;
+      for (int i = 0; i < model.lengths.length; i++) {
+        int[] hashStringClone = model.hashString.clone();
+        int b = model.currentPosition + model.lengths[i] - 1;
+        if (b >= model.hashString.length) {
+          b = b - model.hashString.length;
         }
 
-        while (count < 64) {
-            for (int i = 0; i < lengths.length; i++) {
-
-                int[] hashStringClone = hashString.clone();
-
-                int b = currentPosition + lengths[i] - 1;
-                if (b >= hashString.length) {
-                    b = b - hashString.length;
-                }
-                for (int step = 0, c = currentPosition; step < lengths[i]; step++, c++, b--) {
-                    if (b == -1) {
-                        b = hashString.length - 1;
-                    }
-                    if (c == hashString.length) {
-                        c = 0;
-                    }
-                    hashString[c] = hashStringClone[b];
-                }
-                currentPosition += lengths[i] + skipSize;
-                skipSize++;
-
-                while (currentPosition >= hashString.length) {
-                    currentPosition = currentPosition - hashString.length;
-                }
-            }
-            count++;
+        for (int step = 0, c = model.currentPosition; step < model.lengths[i]; step++, c++, b--) {
+          if (b == -1) {
+            b = model.hashString.length - 1;
+          }
+          if (c == model.hashString.length) {
+            c = 0;
+          }
+          model.hashString[c] = hashStringClone[b];
         }
 
-        int[] sparseHash = new int[16];
-        for (int hash = 0; hash < 16; hash++) {
-            int value = 0;
-            for (int i = (hash * 16), j = (hash * 16) + 16; i < j; i++) {
-                value = value ^ hashString[i];
-            }
-            sparseHash[hash] = value;
+        model.currentPosition += model.lengths[i] + model.skipSize;
+        model.skipSize++;
 
+        while (model.currentPosition >= model.hashString.length) {
+          model.currentPosition = model.currentPosition - model.hashString.length;
         }
+      }
+    }
 
-        StringBuilder buffer = new StringBuilder();
-        for (int v : sparseHash) {
-            String h = Integer.toHexString(v);
-            buffer.append(h.length() == 1 ? ("0" + h) : h);
-        }
+    class Model {
 
-        return buffer.toString();
+      private int[] lengths;
+      private int[] hashString;
+
+      private int currentPosition;
+      private int skipSize;
+
+      public int[] getLengths() {
+        return this.lengths;
+      }
+
+      public void setLengths(int[] lengths) {
+        this.lengths = lengths;
+      }
+
+      public int[] getHashString() {
+        return this.hashString;
+      }
+
+      public void setHashString(int[] hashString) {
+        this.hashString = hashString;
+      }
+
+      public int getCurrentPosition() {
+        return this.currentPosition;
+      }
+
+      public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+      }
+
+      public int getSkipSize() {
+        return this.skipSize;
+      }
+
+      public void setSkipSize() {
+        this.skipSize = skipSize;
+      }
     }
 }
